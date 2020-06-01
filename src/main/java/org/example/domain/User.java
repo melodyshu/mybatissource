6d409f30414635c0e6ed901e9277386cdc847818
huangzhimao <huangzhimao@cestc.cn>
2020-06-01 10:13:00 +0800

    加入lombok

---
+++ src/main/java/org/example/domain/User.java
@@ -0,0 +1,15 @@
+package org.example.domain;
+
+import lombok.Data;
+
+/**
+ * @Author: huangzhimao
+ * @Date: 2020-06-01
+ * @Description:
+ */
+@Data
+public class User {
+
+  private String name;
+  private String address;
+}
